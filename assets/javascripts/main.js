229bfad880ecd2a2f4229a9bf0d18a278945faac
Greg Hunt <freshbrewedweb@gmail.com>
2017-06-18 20:39:22 -0400

    Added portfolio to designs page

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     $("#menuToggle").on('click', function(){
         $("body").toggleClass("navigation--is-open");
     });
+
+    $(".designs a").on("click", function(e){
+        e.preventDefault();
+
+        $(".design-explanation").slideUp(200);
+        $(".designs a").removeClass("design--active");
+        var target = $(this).attr("href");
+
+        if( !$(this).hasClass("design--active") && !$(target).is(":visible") ) {
+            $(this).addClass("design--active");
+            $( target ).slideDown(200);
+        }
+    });
 });
